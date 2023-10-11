a39b05397392f4836ca3eba060ce2e1204a59af4
Tóth Erik <totherikjanos@gmail.com>
2023-10-11 07:11:01 +0200

    stashed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -12,79 +12,73 @@ import Settings from "./components/settings";
 import JoinUs from "./components/join";
 import AboutUs from "./components/about";
 import FoodSavers from "./components/foodSavers";
-<<<<<<< Updated upstream
 import Map from "./components/map";
-=======
 import { AuthContextProvider } from "./components/AuthContext";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { Role } from "./misc/Role.enum";
->>>>>>> Stashed changes
+
 
 const rootElement = document.getElementById("root");
 ReactDOM.createRoot(rootElement).render(
-    <ChakraProvider>
-<<<<<<< Updated upstream
-      <Router>
+  <ChakraProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<FoodSavers />}></Route>
+        <Route path="/foodsavers" element={<FoodSavers />}></Route>
+        <Route path="/aboutus" element={<AboutUs />}></Route>
+        <Route path="/join" element={<JoinUs />}></Route>
+      </Routes>
+    </Router>
+    <BrowserRouter>
+      <AuthContextProvider>
         <Routes>
+
           <Route path="/" element={<FoodSavers />}></Route>
-          <Route path="/foodsavers" element={<FoodSavers />}></Route>
           <Route path="/aboutus" element={<AboutUs />}></Route>
+          <Route path="/foodsavers" element={<FoodSavers />}></Route>
           <Route path="/join" element={<JoinUs />}></Route>
-          <Route path="/loggedin" element={<Simple />}></Route>
-          <Route path="/loggedin/upload" element={<FoodUpload />}></Route>
-          <Route path="/loggedin/uploaded" element={<FoodUploaded />}></Route>
-          <Route path="/loggedin/search" element={<FoodSearch />}></Route>
-          <Route path="/loggedin/spec" element={<NeedsSpec />}></Route>
-          <Route path="/loggedin/settings" element={<Settings />}></Route>
-          <Route path="/loggedin/map" element={<Map />}></Route>
+
+          <Route path="/loggedin" element={
+            <ProtectedRoute accessBy="authenticated">
+              <Simple />
+            </ProtectedRoute>
+          } />
+
+          <Route path="/loggedin/upload" element={
+            <ProtectedRoute accessBy="authenticated" roles={[Role.FOOD_OFFERER]}>
+              <FoodUpload />
+            </ProtectedRoute>}
+          />
+
+          <Route path="/loggedin/uploaded" element={
+            <ProtectedRoute accessBy="authenticated">
+              <FoodUploaded />
+            </ProtectedRoute>}
+          />
+          <Route path="/loggedin/search" element={
+            <ProtectedRoute accessBy="authenticated">
+              <FoodSearch />
+            </ProtectedRoute>
+          } />
+          <Route path="/loggedin/spec" element={
+            <ProtectedRoute accessBy="authenticated">
+              <NeedsSpec />
+            </ProtectedRoute>
+          } />
+          <Route path="/loggedin/settings" element={
+            <ProtectedRoute accessBy="authenticated">
+              <Settings />
+            </ProtectedRoute>
+          } />
+
+          <Route path="/loggedin/map" element={
+            <ProtectedRoute accessBy="authenticated">
+              <Map />
+            </ProtectedRoute>
+          } />
+
         </Routes>
-      </Router>
-=======
-      <BrowserRouter>
-        <AuthContextProvider>
-          <Routes>
-
-            <Route path="/" element={<FoodSavers />}></Route>
-            <Route path="/aboutus" element={<AboutUs />}></Route>
-            <Route path="/foodsavers" element={<FoodSavers />}></Route>
-            <Route path="/join" element={<JoinUs />}></Route>
-
-            <Route path="/loggedin" element={
-              <ProtectedRoute accessBy="authenticated">
-                <Simple />
-              </ProtectedRoute>
-            } />
-
-            <Route path="/loggedin/upload" element={
-              <ProtectedRoute accessBy="authenticated" roles={[Role.FOOD_OFFERER]}>
-                <FoodUpload />
-              </ProtectedRoute>}
-            />
-
-            <Route path="/loggedin/uploaded" element={
-              <ProtectedRoute accessBy="authenticated">
-                <FoodUploaded />
-              </ProtectedRoute>}
-            />
-            <Route path="/loggedin/search" element={
-              <ProtectedRoute accessBy="authenticated">
-                <FoodSearch />
-              </ProtectedRoute>
-            } />
-            <Route path="/loggedin/spec" element={
-              <ProtectedRoute accessBy="authenticated">
-                <NeedsSpec />
-              </ProtectedRoute>
-            } />
-            <Route path="/loggedin/settings" element={
-              <ProtectedRoute accessBy="authenticated">
-                <Settings />
-              </ProtectedRoute>
-            } />
-
-          </Routes>
-        </AuthContextProvider>
-      </BrowserRouter>
->>>>>>> Stashed changes
-    </ChakraProvider>
+      </AuthContextProvider>
+    </BrowserRouter>
+  </ChakraProvider>
 );
